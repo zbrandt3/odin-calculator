2223fa704d42a7c9c3a90057835d426a3aa34cb4
zbrandt3 <zbrandt3@gmail.com>
2024-12-19 15:07:52 -0600

    add basic number button functionality

--- script.js
+++ script.js
@@ -1,5 +1,48 @@
+const numKeys = document.querySelectorAll(".numkey");
+const operatorKeys = document.querySelectorAll(".operator-key");
+const display = document.querySelector(".display");
+const clearBtn = document.querySelector("#clear");
+let operator = "";
+let val1 = ""; 
+let val2 = "";
+
+
+numKeys.forEach(numkey=> {
+    numkey.addEventListener("click", numClick)
+});
+operatorKeys.forEach(operatorKey => {
+    operatorKey.addEventListener("click", operatorClick)
+})
+
+clearBtn.addEventListener("click", ()=> {
+    display.innerText = 0;
+})
+
+function operatorClick() {
+    val1 = display.innerText;
+    operator = this.innerText;
+    console.log(val1, operator);
+
+}
+
+function numClick() {
+    if(display.innerText.length >= 14) { return; }
+    if(display.innerText == 0) { display.innerText = ""; }
+
+    if(operator === "") {
+        console.log(this);
+        display.innerText += this.innerText;
+    }
+
+    if(operator !== "") {
+        operator = "";
+        display.innerText="";
+        display.innerText += this.innerText;
+    }
+}
+
 function add(a, b) {
-    return a + b;
+    return Number(a) + Number(b);
 }
 
 function subtract(a, b) {
