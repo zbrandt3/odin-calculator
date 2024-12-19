3373abcb2bc872c8b0a9f58d70e2f8bde6d22dab
zbrandt3 <zbrandt3@gmail.com>
2024-12-18 21:08:09 -0600

    add basic calculator functions Added add() subtract() multiply() divide() operate()

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-function add() {
-
+function add(a, b) {
+    return a + b;
 }
 
-function subtract() {
-
+function subtract(a, b) {
+    return a - b;
 }
 
-function multiply() {
-
+function multiply(a, b) {
+    return a * b;
 }
 
-function divide() {
+function divide(a, b) {
+    return a !== 0 ? a / b : "ERR";
+}
 
+function operate(num1, num2, operator) {
+    if(operator === "+") { return add(num1, num2) }
+    if(operator === "-") { return subtract(num1, num2) }
+    if(operator === "*") { return multiply(num1, num2) }
+    if(operator === "/") { return divide(num1, num2) }
 }
